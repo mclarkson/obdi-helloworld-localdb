d3d3d5cff508feb09a72dbb62d68310c87c1c419
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-04-07 18:00:25 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,53 @@
+# obdi-helloworld-localdb
+
+This Obdi plugin has a UI and a private sqlite3 database.
+
+## Screenshot
+
+![](images/helloworld-localdb.png?raw=true)
+
+## What is it?
+A starting point for writing an Obdi plugin that uses a private sqlite3 database.
+
+* This plugin has a Web Interface.
+* This plugin uses a private sqlite database.
+* This plugin exposes a REST endpoint.
+    The GUI uses REST to access the private database.
+* This plugin does not run scripts on workers.
+
+Similar plugin: [obdi-saltregexmanager](https://github.com/mclarkson/obdi-saltregexmanager)
+
+## Using the REST Endpoint for CRUD from the Command Line
+
+```
+# Log in
+
+$ ipport="127.0.0.1:443"
+
+$ guid=`curl -ks -d '{"Login":"nomen.nescio","Password":"password"}' \
+  https://$ipport/api/login | grep -o "[a-z0-9][^\"]*"`
+
+# Create
+
+$ curl -k -d '{"Text":"Hello"}' \
+  https://$ipport/api/nomen.nescio/$guid/helloworld-localdb/helloworld-localdb?env_id=1
+
+# Read
+
+$ curl -k https://$ipport/api/nomen.nescio/$guid/helloworld-localdb/helloworld-localdb?env_id=1
+
+# Update
+
+$ curl -k -d '{"Id":1,"Text":"Hello there"}' -X PUT \
+  https://$ipport/api/nomen.nescio/$guid/helloworld-localdb/helloworld-localdb?env_id=1
+
+# Delete
+
+$ curl -k -X DELETE \
+  https://$ipport/api/nomen.nescio/$guid/helloworld-localdb/helloworld-localdb/1?env_id=1
+```
+
+The job and its output can also be viewed using the System Jobs viewer.
+
+See [obdi-dev-repository](https://github.com/mclarkson/obdi-dev-repository)
+for more information about Dev plugins.
---
+++ install_plugin.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Obdi - a REST interface and GUI for deploying software
+# Copyright (C) 2014  Mark Clarkson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# helloworld-runscript plugin
+
+[[ -z $guid ]] && {
+  echo "ERROR: environment variable 'guid' must be set."
+  exit 1
+}
+
+proto="https"
+opts="-k -s" # don't check ssl cert, silent
+ipport="127.0.0.1:443"
+
+#
+# Create a temporary file and a trap to delete it
+#
+
+t="/tmp/install_helloworld_localdb_$$"
+touch $t
+[[ $? -ne 0 ]] && {
+    echo "Could not create temporary file. Aborting."
+    exit 1
+}
+trap "rm -f -- '$t'" EXIT
+
+#
+# Create the plugin entry in obdi, so it can be shown in the sidebar
+#
+
+curl -k -d '{
+    "Name":"helloworld-localdb",
+    "Desc":"Hello World plugin.",
+    "HasView":1,
+    "Parent":""
+}' $proto://$ipport/api/admin/$guid/plugins | tee $t
+
+# Grab the id of the last insert
+id=`grep Id $t | grep -Eo "[0-9]+"`
+
+#
+# Add the AJS controller files
+#
+# These need to be loaded when the application starts
+#
+
+curl -k -d '{
+    "Name":"helloworld-localdb.js",
+    "Desc":"Controller for HelloWorld LocalDB",
+    "Type":1,
+    "PluginId":'"$id"',
+    "Url":"helloworld-localdb/js/controllers/helloworld-localdb.js"
+}' $proto://$ipport/api/admin/$guid/files
+
+# Delete the temporary file and delete the trap
+rm -f -- "$t"
+trap - EXIT
+
